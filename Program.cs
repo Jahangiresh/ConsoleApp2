d604cee7c461d174fe22048ac2fb7427ca0a1149
Jahangiresh <98384389+Jahangiresh@users.noreply.github.com>
2022-02-13 17:46:44 +0400

    monday task
    tasssskkkkk

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+//static void checkNumberIfNumberDevidedSeven(int num)
+//{
+//    int q = num % 7;
+//    int t = 7 - q;
+//    if (q == 0)
+//    {
+//        Console.WriteLine("divided by 7");
+//    }
+//    else if (q <= 3)
+//    {
+//        num -= q;
+//        Console.WriteLine(num);
+//    }
+//    else
+//    {
+//        num += t;
+//        Console.WriteLine(num);
+//    }
+//}
+
+
+//int number =Convert.ToInt32(Console.ReadLine());
+
+//checkNumberIfNumberDevidedSeven(number);
+
+//static void SimpleMath(int num1,char arithmetics, int num2)
+//{
+
+//    int result = 0;
+
+//    switch (arithmetics)
+//    {
+//        case '*':
+//            result = num1 * num2;
+//            break;
+//        case '/':
+//            result = num1 / num2;
+//            break;
+//        case '-':
+//            result = num1 - num2;
+//            break;
+//        case '+':
+//            result = num1 + num2;
+//            break;
+//    }
+//    Console.WriteLine(result);
+//}
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//char arithmetics = Convert.ToChar(Console.ReadLine());
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//SimpleMath(num1, arithmetics, num2);
