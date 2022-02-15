4d04122fe6829d95ba63d3ec87eb848960559457
Jahangiresh <98384389+Jahangiresh@users.noreply.github.com>
2022-02-15 09:50:28 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,56 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace TetsForMethods
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            //int[] grades = { 70, 65, 100, 90, 30, 95, 40 };
-
-
-            //int result = subs(grades);
-
-
-
-            //Console.WriteLine(result);
-
-            int grades = 490;
-           
-            
-            int bal = average(grades);
-
-            Console.WriteLine(bal);
-
-
-
-
+            int[] grades = { 70, 65, 100, 90, 30, 95, 40 };
+            int studs;
+            int result = subs(grades, out studs);
+            int orta = average(result, studs);
+            Console.WriteLine(orta);
 
 
         }
-        //public static int subs(int[] grades)
-        //{
-        //    int result = 0;
-        //    int studs = 0;
-
-        //    foreach (int grade in grades)
-        //    {
-        //        result += grade;
-        //        studs++;
-        //    }
-        //    return result;
-        //}
-        public static int average( int grades)
+        public static int subs(int[] grades, out int studs)
         {
-            int studs = 7;
-            int orta =grades/studs;
-             return orta;
-            
-            
+            int result = 0;
+            studs = grades.Length;
+
+            foreach (int grade in grades)
+            {
+                result += grade;
+
+            }
+            return result;
+        }
+        public static int average(int result, int studs)
+        {
+            int orta = result / studs;
+            return orta;
+
+
         }
 
     }
