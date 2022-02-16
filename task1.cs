aaa6464d7d41e237c7d60579f627d5ebb23b88ba
Jahangiresh <98384389+Jahangiresh@users.noreply.github.com>
2022-02-16 12:23:49 +0400

    Update and rename task1.csproj to task1.cs

---
+++ task1.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "C SHarp Sucks";
+            string bb = "";
+
+            for (int i = 0; i < a.Length; i++)
+            {
+
+                if (char.IsUpper(a[i]))
+                {
+                    bb += char.ToLower(a[i]);
+                }
+                else
+                {
+                    bb += char.ToUpper(a[i]);
+
+                }
+
+            }
+            Console.WriteLine(bb);
+        }
+    }
+}
